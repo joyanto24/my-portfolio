cb755c85cdf23c37635581ec905551b5be1b6667
joyanto24 <joytalukder1234@gmail.com>
2024-11-09 21:47:34 +0600

    my portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config ={
+    theme:{
+        gridTemplateColumns:{
+            'auto': 'repeat(auto-fit, minmax(200px,1fr))'
+        },
+        fontFamily:{
+            Outfit:["Outfit", "sans-serif"],
+            Ovo:["Ovo", "serif"]
+        },
+        animation:{
+            spin_slow:'spin 6s linear infinite'
+        }
+    }
+}
